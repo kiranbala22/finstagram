cb40c81228bf317e73fffd51e52acf0119a87540
Kiran Bala <kiranbala22@gmail.com>
2016-09-21 04:02:54 +0000

    get ruby basic

--- app/actions.rb
+++ app/actions.rb
@@ -1,42 +1,46 @@
 def humanized_time_ago(time_ago_in_minutes)
     if time_ago_in_minutes >= 60
-        #"{time_ago_in_minutes / 60} hours ago"
+        "#{time_ago_in_minutes / 60 } hours ago"
     else
-        #"{time_ago_in_minutes} minutes ago"
+        "#{time_ago_in_minutes} minutes ago"
     end
 end
-
-
-
 get '/' do
-    
-  post = {
-      username: "sharky_j",
+  post_shark = {
+  username: "sharky_j",
   avatar_url: "http://naserca.com/images/sharky_j.jpg",
   photo_url: "http://naserca.com/images/shark.jpg",
-  time_ago_in_minutes: 15,
+  humanized_time_ago: humanized_time_ago(15),
   like_count: 0,
   comment_count: 1,
-  comments: [
-    "sharky_j: Out for the long weekend... too embarrassed to show y'all the beach bod!"
-  ]
-  humanized_time_ago(post[:time_ago_in_minutes])
+  comments: [{
+    username: "sharky_j", 
+    text: "Out for the long weekend... too embarrassed to show y'all the beach bod!"
+  }]
+    }
+    post_whale = {
+    username: "kirk_whalum",
+    avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
+    photo_url: "http://naserca.com/images/whale.jpg",
+    humanized_time_ago: humanized_time_ago(65),
+    like_count: 0,
+    comment_count: 1,
+    comments: [{
+      username: "kirk_whalum",
+      text: "#weekendvibes"
+    }]
   }
-  end
-  # if time_ago_in_minutes is more than 60
-  if time_ago_in_minutes > 60
-      #"{time_ago_in_minutes / 60} hours ago"
-      # return this string
-      "more than an hour"
-      #if it is less than or equal
-      elsif time_ago_in_minutes == 60
-      "an hour"
-      elsif time_ago_in_minutes <= 1
-      "just a moment ago"
-          
-  else
-      #"{time_ago_in_minutes} minutes ago"
-      # return this instead
-      "less than an hour"
-  end
+  post_marlin = {
+    username: "marlin_peppa",
+    avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
+    photo_url: "http://naserca.com/images/marlin.jpg",
+    humanized_time_ago: humanized_time_ago(190),
+    like_count: 0,
+    comment_count: 1,
+    comments: [{
+      username: "marlin_peppa",
+      text: "lunchtime! ;)"
+    }]
+  }
+  [post_shark, post_whale, post_marlin].to_s
 end
