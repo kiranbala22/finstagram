e77772fca2d50a7f652a9b120b27df8ed536c10a
Kiran Bala <kiranbala22@gmail.com>
2016-10-05 04:37:50 +0000

    get my form setup

--- app/actions.rb
+++ app/actions.rb
@@ -6,5 +6,28 @@ get '/' do
    erb(:index)
 end
  
+get '/signup' do
+    
+  @user = User.new
+    
+    erb(:signup)
+end
+    
+post '/signup' do
+    
+    # grab user input values from params
+    email       = params[:email]
+    avatar_url  = params[:avatar_url]
+    username    = params[:username]
+    password    = params[:password]
+    
+    #instantiate and save a User
+    user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+    user.save
+    
+    #return readable representation of User object
+    
+    escape_html user.inspect
 
+end    
   
