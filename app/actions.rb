e51773342cbfe4a09c4cfd7d39747b73fd10d78b
Kiran Bala <kiranbala22@gmail.com>
2016-10-06 21:16:27 +0000

    get my login, session and redirect setup.

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,8 @@
+helpers do
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+end    
 
 get '/' do
   
@@ -26,13 +31,48 @@ post '/signup' do
     
     #if user validation pass and user is saved
     if @user.save
-        "User #{username} saved!"
+        redirect to('/login')
     
     else
     # display simple error message
    
     erb(:signup)
     end
+end
+get '/login' do
+    
+    erb(:login)    
+    
+end
 
- end   
+post '/login' do
+    
+    username    = params[:username]
+    password    = params[:password]
+    
+    #find user by username
+    
+    user = User.find_by(username: username)
+    
+    #if that user exists
+    if user && user.password == password
+        session[:user_id] = user.id
+            redirect to('/')
+        else
+        @error_message = "Login failed."
+        
+        erb(:login)
+    end
+    
+end    
+
+get '/logout' do
+    
+    session[:user_id] = nil
+    
+    redirect to('/')
+end    
+    
+    
+   
   
